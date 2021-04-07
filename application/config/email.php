44ab7f47c58c03ba892695616a70dd8328e13be0
Sauravqq <sauravdeshmukh11@gmail.com>
2021-04-07 13:08:41 +0530

    Xento-Project

---
+++ application/config/email.php
@@ -0,0 +1,8 @@
+<?php
+// $config = array();
+// $config['protocol'] = 'smtp';
+// $config['smtp_host'] = 'ssl://smtp.gmail.com';
+// $config['smtp_user'] = 'ms6312328@gmail.com';
+// $config['smtp_pass'] = '242%$#@2()';
+// $config['smtp_port'] = '465';
+?>
